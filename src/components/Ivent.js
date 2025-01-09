1c5917e67d04fb5c37e2a038c6b738a1344e362f
Mobin-ej <mobinyekew82@gmail.com>
2025-01-09 14:53:58 +0330

    form

--- src/components/Ivent.js
+++ src/components/Ivent.js
@@ -32,8 +32,8 @@ const Ivent = () => {
     const { title, description, date, person_in_charge, time, address } = eventData;
 
     return (
-        <div className="card relative flex items-center justify-center w-[90%] md:w-[80%] h-[200px] md:h-[254px] overflow-hidden rounded-2xl">
-            <h2 className="z-10 text-slate-400 text-3xl md:text-5xl text-right">{title}</h2>
+        <div className="card relative  flex items-center justify-center w-[90%] md:w-[80%] h-[200px] md:h-[254px] overflow-hidden rounded-2xl">
+            <h2 className="z-10  text-gray-400 text-3xl md:text-5xl font-BNazanin text-right">{title}</h2>
             <div className="absolute inset-0 flex justify-center items-center">
                 <div className="bg-gradient-to-b from-[#00B7FF] to-[#FF30FF] w-[80px] h-[300%] md:w-[100px] md:h-[380%] animate-rotateBG absolute"></div>
                 <div className="absolute inset-[5px] bg-[#DFF2EB] rounded-lg p-4 text-right">
